c516a72f220e20670decf467fc6d847426af554d
wu yuntao <wyt.py@thonic.org>
2008-11-25 12:36:17 +0000

    Added restriction to URLs

--- examples/proxy.py
+++ examples/proxy.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import cgi
+import re
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -17,15 +18,19 @@ class DoubanProxy(webapp.RequestHandler):
     def delete(self):
         proxy(self)
 
+re_douban = re.compile(r'^http:\/\/(api|www)+\.douban\.com\/.*')
 
 def proxy(handler):
     url = handler.request.get('url')
-    header = { 'Authorization': handler.request.headers.get('Authorization', '') }
-    payload = None
-    if handler.request.method is 'POST' or handler.request.method is 'PUT':
-        payload = handler.request.body
-    response = urlfetch.fetch(payload=payload, url=url, method=handler.request.method, headers=header)
-    handler.response.out.write(response.content)
+    if re_douban.match(url):
+        header = { 'Authorization': handler.request.headers.get('Authorization', '') }
+        payload = None
+        if handler.request.method is 'POST' or handler.request.method is 'PUT':
+            payload = handler.request.body
+        response = urlfetch.fetch(payload=payload, url=url, method=handler.request.method, headers=header)
+        handler.response.out.write(response.content)
+    else:
+        handler.response.out.write("Invalid URL")
 
 # URL patterns
 urls = [
