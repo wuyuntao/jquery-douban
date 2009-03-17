aa62c543d3652d5f4fb4198e6392af9489aa7ef4
wu yuntao <wyt.py@thonic.org>
2009-03-18 00:55:23 +0800

    New gears handler

--- src/gears_handler.js
+++ src/gears_handler.js
@@ -1,50 +1,73 @@
-/* 
- * Gears request handler for jQuery Douban
- *
- * Copyright (c) 2008 Wu Yuntao <http://blog.luliban.com/>
- * Licensed under the Apache 2.0 license.
- *
- */
-var gearsHandler = function(s) {
-    if (!google || !google.gears) return;
-    s = $.extend({}, $.douban.http.settings, s || {});
+if (google && google.gears) {
 
-    // Convert data if not already a string
-    if (s.data && typeof s.data != "string") s.data = $.param(s.data);
-
-    // If data is available, append data to url for get requests
-    if (s.data && s.type == "GET" || s.type == "DELETE") {
-        s.url += (s.url.match(/\?/) ? "&" : "?") + s.data;
-        s.data = null;
+var Douban = {};
+Douban.handler = {};
+Douban.util = {
+    buildUri: function(url, params, proxy) {
+        url += (/\?/.test(url) ? '&' : '?') + $.param(params);
+        if (proxy) url = proxy + encodeURIComponent(url);
+        return url
     }
-
-    // Set proxy for url if proxy URL is available
-    if (gearsHandler.proxyUrl)
-        s.url = gearsHandler.proxyUrl + encodeURIComponent(s.url);
-
-    // Set Content-Type header
-    if (s.data)
-        $.extend(s.headers, { 'Content-Type': s.contentType });
-
-    // JavaScript function cannot be marshaled in child worker
-    var success = s.success;
-    s.success = null;
-
-    // Setup the WorkerPool
-    var workerPool = google.gears.factory.create('beta.workerpool');
-
-    // Handle on success
-    workerPool.onmessage = function(a, b, message) {
-        var data = message.body;
-        if (data && s.dataType == 'json') data = eval("(" + data + ")");
-        success(data);
-    };
-
-    // Create child worker and sent message to her
-    var childWorkerId = workerPool.createWorkerFromUrl(gearsHandler.workerUrl);
-    workerPool.sendMessage(s, childWorkerId);
 };
 
-gearsHandler.name = 'gears';
-gearsHandler.proxyUrl = '/proxy?url=';
-gearsHandler.workerUrl = '/worker.js';
+var GearsHandler = Douban.handler.gears = {
+    name: 'gears',
+    proxy: null,
+
+    GET: function(url, params, headers, success) {
+        var url = Douban.util.buildUri(url, params, GearsHandler.proxy)
+        var req = google.gears.factory.create('beta.httprequest');
+        GearsHandler.setStateChange(req, 'json', success);
+        req.open('GET', url);
+        GearsHandler.setHeaders(req, headers);
+        req.send();
+    },
+
+    POST: function(url, params, data, headers, success) {
+        var url = Douban.util.buildUri(url, params, GearsHandler.proxy)
+        var req = google.gears.factory.create('beta.httprequest');
+        GearsHandler.setStateChange(req, 'json', success);
+        req.open('POST', url);
+        headers['Content-Type'] = 'application/atom+xml';
+        GearsHandler.setHeaders(req, headers);
+        req.send(data);
+    },
+
+    PUT: function(url, params, data, headers, success) {
+        var url = Douban.util.buildUri(url, params, GearsHandler.proxy)
+        var req = google.gears.factory.create('beta.httprequest');
+        headers['Content-Type'] = 'application/atom+xml';
+        GearsHandler.setStateChange(req, 'json', success);
+        req.open('PUT', url);
+        GearsHandler.setHeaders(req, headers);
+        req.send(data);
+    },
+
+    DELETE: function(url, params, headers, success) {
+        var url = Douban.util.buildUri(url, params, GearsHandler.proxy)
+        var req = google.gears.factory.create('beta.httprequest');
+        GearsHandler.setStateChange(req, 'text', success);
+        req.open('DELETE', url);
+        GearsHandler.setHeaders(req, headers);
+        req.send();
+    },
+
+    setStateChange: function(request, type, success) {
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 &&
+                request.status >= 200 && request.status < 304) {
+                var data = request.responseText;
+                if (type == 'json') data = eval('(' + data + ')');
+                success(data);
+            }
+        }
+    },
+
+    setHeaders: function(req, headers) {
+        for (var name in headers)
+            req.setRequestHeader(name, headers[name]);
+    }
+
+};
+
+}
