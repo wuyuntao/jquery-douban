0814dc87b8f71e2ee7dabf504d497bc9025690b4
wu yuntao <wyt.py@thonic.org>
2009-03-18 12:38:52 +0800

    Refactor OAuth client testcases

--- apps/media/tests/oauth.js
+++ apps/media/tests/oauth.js
@@ -1,114 +1,80 @@
 module("OAuth Client Testcases");
 
 test("test factory method", function() {
-    var client = $.douban('client', { key: '1', secret: '2' });
+    var client = new Client({
+        key: '1', secret: '2',
+        request: Douban.handler.jquery
+    });
     equals(client.api.key, '1', "api key expected to be 1");
     equals(client.api.secret, '2', "api secret expected to be 2");
-    equals(client._http.name, 'jquery', "http type expected to be \"jquery\"");
+    equals(client.request.name, 'jquery', "http type expected to be \"jquery\"");
 });
 
-test("test authorization step 1: get request token synchronously", function() {
+test("test authorization step 1: get request token", function() {
     if (typeof netscape != 'undefined')
         netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
 
-    var client = $.douban('client', {
+    var client = new Client({
         key: '0107c5c3c9d4ecc40317514b5d7ec64c',
         secret: '7feaf4ec7b6989f8'
     });
 
-    var requestToken = client.getRequestToken();
-    equals(requestToken.key.length, 32, "check the length of request key ( \"" + requestToken.key + "\" )");
-    equals(requestToken.secret.length, 16, "check the length of request secret ( \"" + requestToken.secret + "\" )");
+    stop();
+    client.getRequestToken(function(token) {
+        equals(token.key.length, 32,
+               "check the length of request key ( \"" + token.key + "\" )");
+        equals(token.secret.length, 16,
+               "check the length of request secret ( \"" + token.secret + "\" )");
+        start();
+    });
     
     // Print authorization URL
     // console.debug(client.getAuthorizationUrl(requestToken));
 });
 
-test("test authorization step 1: get request token asynchronously", function() {
-    if (typeof netscape != 'undefined')
-        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-
-    var client = $.douban('client', {
-        key: '0107c5c3c9d4ecc40317514b5d7ec64c',
-        secret: '7feaf4ec7b6989f8',
-        async: true
-    });
-
-    stop();
-    client.getRequestToken(function(token) {
-        equals(token.key.length, 32, "check the length of request key ( \"" + token.key + "\" )");
-        equals(token.secret.length, 16, "check the length of request secret ( \"" + token.secret + "\" )");
-        start();
-    });
-});
-
 test("test authorization step 2: get authorization url", function() {
-    var client = $.douban('client', { key: '1', secret: '2' });
+    var client = new Client({ key: '1', secret: '2' });
     var callback = 'mycallback';
-    // Set request token manually
-    client.requestToken = { key: '3', secret: '4' };
-
-    var url = client.getAuthorizationUrl(callback);
+    var url = client.getAuthorizationUrl({ key: '3', secret: '4' }, callback);
     equals(url, 'http://www.douban.com/service/auth/authorize?oauth_token=3&oauth_callback=mycallback');
-
-    var url2 = client.getAuthorizationUrl({ key: '5', secret: '6' });
-    equals(url2, 'http://www.douban.com/service/auth/authorize?oauth_token=5&oauth_callback=');
 });
 
-test("test authorization step 3: get access token synchronously", function() {
+test("test authorization step 3: get access token", function() {
     if (typeof netscape != 'undefined')
         netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
 
-    var client = $.douban('client', {
+    var client = new Client({
         key: '0107c5c3c9d4ecc40317514b5d7ec64c',
         secret: '7feaf4ec7b6989f8'
     });
-    requestToken = { key: '', secret: '' };
+    requestToken = {
+        key: '35856283b122f38ba090278b1368daa1',
+        secret: '7b85d5bb9fc0d167'
+    };
 
-    if (requestToken.key) {
-        var accessToken = client.getAccessToken(requestToken);
+    /*
+    stop();
+    client.getAccessToken(requestToken, function(token, uid) {
         // When the client is authenticated, the request token will be invalid
-        equals(accessToken.key.length, 32, "check the length of access key ( \"" + accessToken.key + "\" )");
-        equals(accessToken.secret.length, 16, "check the length of access secret ( \"" + accessToken.secret + "\" )");
-        equals(client.userId, '1139389', "get username");
-    }
-});
-
-test("test authorization step 3: get access token asynchronously", function() {
-    if (typeof netscape != 'undefined')
-        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-
-    var client = $.douban('client', {
-        key: '0107c5c3c9d4ecc40317514b5d7ec64c',
-        secret: '7feaf4ec7b6989f8',
-        async: true
+        equals(token.key.length, 32,
+               "check the length of access key ( \"" + token.key + "\" )");
+        equals(token.secret.length, 16,
+               "check the length of access secret ( \"" + token.secret + "\" )");
+        equals(uid, '1139389', "get username");
+        start();
     });
-    requestToken = { key: '', secret: '' };
-
-    if (requestToken.key) {
-        stop();
-        client.getAccessToken(requestToken, function(token, userId) {
-            // When the client is authenticated, the request token will be invalid
-            equals(token.key.length, 32, "check the length of access key ( \"" + token.key + "\" )");
-            equals(token.secret.length, 16, "check the length of access secret ( \"" + token.secret + "\" )");
-            equals(client.userId, '1139389', "get username");
-            start();
-        });
-    }
+    */
 });
 
 test("test programmatic login", function() {
-    var client = $.douban('client', { key: '1', secret: '2' });
-    var accessToken = { key: '6fcf833aff0589883f6e89b0fd109c98', secret: 'b692646c5d1929ab' };
+    var client = new Client({
+        key: '0107c5c3c9d4ecc40317514b5d7ec64c',
+        secret: '7feaf4ec7b6989f8'
+    });
+    var accessToken = {
+        key: '2e2fa5e45bee84308a8ef1ad13cb8c90',
+        secret: 'fe5e5256ee3128a1'
+    };
     var login = client.login(accessToken);
     ok(login, "login successful");
-    ok(client.isAuthenticated(), "access is authenticated");
 });
-
-test("test auth headers", function() {
-    var client = $.douban('client', { key: '1', secret: '2' });
-    var headers = client.getAuthHeaders('http://api.douban.com/people/1000001', 'GET', { 'alt': 'json' });
-    ok(headers, "get headers ( \"" + headers + "\" )");
-});
-
-// vim: foldmethod=indent
