62f4cc7faf9d5a7aa400f60f3080e8010ccc99dd
wu yuntao <wyt.py@thonic.org>
2009-03-19 12:57:45 +0800

    Refactor miniblog service.

--- apps/media/tests/miniblog.js
+++ apps/media/tests/miniblog.js
@@ -1,53 +1,63 @@
-module("Douban Miniblog Testcases");
+module("广播 API 测试");
 
-/* Only ``getForContacts`` should be tested
- */
-test("test miniblog api", function() {
-    expect(6);
+test("测试获取用户广播", function() {
+    if (typeof netscape != 'undefined')
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+    var service = createService();
+    var re = /http:\/\/api\.douban\.com\/miniblog\/\d+/;
 
+    stop();
+    service.miniblog.getForUser('wyt', 10, 10, function(miniblogs) {
+        console.debug(miniblogs);
+        equals(miniblogs.offset, 10);
+        ok(miniblogs.entry[9].id.match(re), "get miniblog id");
+        start();
+    });
+});
+
+test("测试获取用户友邻广播", function() {
+    if (typeof netscape != 'undefined')
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+    var service = createService();
+    var re = /http:\/\/api\.douban\.com\/miniblog\/\d+/;
+
+    stop();
+    service.miniblog.getForContacts('iloveshutuo', 9, 9, function(miniblogs) {
+        console.debug(miniblogs);
+        equals(miniblogs.limit, 9);
+        ok(miniblogs.entry[5].id.match(re), "get miniblog id");
+        start();
+    });
+});
+
+test("测试添加广播", function() {
     if (typeof netscape != 'undefined')
         netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
     var service = createService();
 
-    /* get miniblog for user
-     */
-    miniblogs = service.miniblog.getForUser('wyt', 10, 10);
-    equals(miniblogs.offset, 10);
-    ok(miniblogs.entries[9].id.match(/http:\/\/api\.douban\.com\/miniblog\/\d+/), "get miniblog id");
-
-    /* get miniblog for contacts
-     */
-    miniblogs2 = service.miniblog.getForContacts('iloveshutuo', 9, 9);
-    equals(miniblogs2.limit, 9);
-    ok(miniblogs2.entries[5].id.match(/http:\/\/api\.douban\.com\/miniblog\/\d+/), "get miniblog id");
-
-    /* add miniblog
-     */
-    miniblog = service.miniblog.add({ content: '真是的，这是什么啊' });
-    equals(miniblog.content, '真是的，这是什么啊');
-
-    /* remove miniblog
-     */
-    var response = service.miniblog.remove(miniblog);
-    ok(response, "miniblog removed");
+    stop();
+    service.miniblog.add({ content: '真是的，这是什么啊' }, function(miniblog) {
+        console.debug(miniblog);
+        equals(miniblog.content, '真是的，这是什么啊');
+        start();
+    });
 });
 
-test("test miniblog object", function() {
-    expect(7);
+test("测试删除广播", function() {
+    if (typeof netscape != 'undefined')
+        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
+    var service = createService();
 
-    var json = {"content":{"$t":"在看康熙卸妆那集，google广告提示“如何去掉脸上的豆豆，豆印”～～","@type":"html"},"category":[{"@scheme":"http://www.douban.com/2007#kind","@term":"http://www.douban.com/2007#miniblog.saying"}],"title":{"$t":"在看康熙卸妆那集，google广告提示“如何去掉脸上的豆豆，豆印”～～"},"id":{"$t":"http://api.douban.com/miniblog/80210749"},"published":{"$t":"2008-11-20T20:43:25+08:00"}};
+    /*
+    stop();
+    service.miniblog.remove("120562453", function(response) {
+       ok(response, "miniblog removed");
+       start();
+    });
+    */
+});
 
-    var miniblog = $.douban('miniblog', json);
-    var date = new Date(2008, 10, 20, 20, 43, 25);
-    equals(miniblog.id, "http://api.douban.com/miniblog/80210749");
-    equals(miniblog.title, "在看康熙卸妆那集，google广告提示“如何去掉脸上的豆豆，豆印”～～");
-    equals(miniblog.content, "在看康熙卸妆那集，google广告提示“如何去掉脸上的豆豆，豆印”～～");
-    equals(miniblog.published.getTime(), date.getTime());
-    equals(miniblog.category, "saying");
-    equals(miniblog.imageUrl, undefined);
-
-    var xml = $.douban.createXml('miniblog', { content: '内容' });
+test("测试生成广播 XML", function() {
+    var xml = Douban.miniblog.createXML({ content: '内容' });
     equals(xml, '<?xml version="1.0" encoding="UTF-8"?><entry xmlns:ns0="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/"><content>内容</content></entry>');
 });
-
-// vim: foldmethod=indent
--- src/miniblog.js
+++ src/miniblog.js
@@ -1,60 +1,34 @@
-/* Douban Miniblog API Service
- * @method      getForUser          获取用户广播
- * @method      getForContact       获取用户友邻广播
- * @method      add                 添加广播
- * @method      remove              删除广播
- */
-var MiniblogService = $.klass(CommonService, {
-    init: function($super, service) {
-        this._model = Miniblog;
-        this._modelEntry = MiniblogEntry;
-        this._getObjectUrl = GET_MINIBLOG_URL;
-        this._addObjectUrl = ADD_MINIBLOG_URL;
-        this._suffix = '/miniblog';
-        // Mask ``get`` and ``update`` method
-        this.get = undefined;
-        this.udpate = undefined;
-        $super(service);
+var MINIBLOG_URL = API_HOST + '/miniblog',
+    GET_MINIBLOG_URL = MINIBLOG_URL + '/{ID}';
+
+// Douban Miniblog API Service
+var Miniblog = Douban.service.miniblog = function(service) {
+    this.service = service;
+};
+Miniblog.prototype = {
+    // 获取广播
+    getForUser: function(miniblog, callback) {
+        this.service.entry(miniblog, callback, Douban.miniblog, GET_MINIBLOG_URL);
     },
-
+    // 获取用户广播
+    getForUser: function(user, offset, limit, callback) {
+        this.service.entry(user, offset, limit, callback, Douban.miniblog, GET_PEOPLE_URL + '/miniblog');
+    },
+    // 获取用户友邻广播
     getForContacts: function(user, offset, limit, callback) {
-        return this._getForObject(user, offset, limit, callback, this._modelEntry, GET_PEOPLE_URL, '/miniblog/contacts');
+        this.service.entry(user, offset, limit, callback, Douban.miniblog, GET_PEOPLE_URL + '/miniblog/contacts');
+    },
+    // 添加广播
+    add: function(data, callback) {
+        this.service.add(data, callback, MINIBLOG_URL + '/saying', Douban.miniblog);
+    },
+    // 删除广播
+    remove: function(miniblog, callback) {
+        this.service.remove(miniblog, callback, GET_MINIBLOG_URL);
     }
-});
-
-/* Douban miniblog class
- * @param           data            Well-formatted json feed
- * @attribute       id              广播ID
- * @attribute       title           广播标题
- * @attribute       content         广播内容
- * @attribute       published       广播发布时间
- * @attribute       category        类别
- * @attribute       type            子类别
- * @attribute       imageUrl        相关图片链接
- * @method          createFromJson  由豆瓣返回的JSON，初始化数据
- */
-var Miniblog = $.klass(DoubanObject, {
-    init: function($super, data) {
-        this.all = ['id', 'title', 'content', 'published', 'category', 'type', 'imageUrl'];
-        $super(data);
-    }
-});
-// Class methods
-/* create POST or PUT xml
- * @param       data, Object
- * @data        content, String
- */
-Miniblog.createXml = function(data) {
-    data = $.extend({ content: '' }, data || {});
-    var xml = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns:ns0="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/"><content>{CONTENT}</content></entry>';
-    return xml.replace(/\{CONTENT\}/, data.content);
 };
 
-/* Douban miniblog entry
- */
-var MiniblogEntry = $.klass(AuthorEntry, {
-    init: function($super, data) {
-        this.model = Collection;
-        $super(data);
-    }
-});
+Douban.miniblog.createXML = function(data) {
+    var xml = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns:ns0="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/"><content>{CONTENT}</content></entry>';
+    return xml.replace(/\{CONTENT\}/, data.content || '');
+};
